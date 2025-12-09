539ad6217907ed1f8c120e4076ccb14a82730df2
chubriki07@gmail.com <chubriki07@gmail.com>
2025-12-09 13:08:42 +1000

    dasdadasd

---
+++ Loshok/DB/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Loshok.DB
+{
+    public class User
+    {
+        public string name {  get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
